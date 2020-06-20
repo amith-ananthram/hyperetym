fed8b5193733bacee594c8efe6af6af6a69e9bb4
Amith Ananthram <amith.ananthram@gmail.com>
2020-06-20 01:46:21 -0700

    add simple gensim training script for etym embeddings

--- etymwordnet.py
+++ etymwordnet.py
@@ -4,8 +4,6 @@ from collections import Counter
 
 DATA_PATH = 'data/etymwn.tsv'
 
-#Counter({'rel:has_derived_form': 2264591, 'rel:is_derived_from': 2264591, 'rel:etymologically_related': 538480, 'rel:etymological_origin_of': 473415, 'rel:etymology': 473415, 'rel:variant:orthography': 16515, 'rel:derived': 2, 'rel:etymologically': 1})
-
 class EtymWordnetRelation(Enum):
 	HAS_DERIVED_FROM = 1
 	IS_DERIVED_FROM = 2
@@ -13,10 +11,30 @@ class EtymWordnetRelation(Enum):
 	ETYMOLOGICAL_ORIGIN_OF = 4
 	VARIANT_ORTHOGRAPHY = 5
 
+	def to_identifier(self):
+		if self == EtymWordnetRelation.HAS_DERIVED_FROM:
+			return 'rel:has_derived_from'
+		elif self == EtymWordnetRelation.IS_DERIVED_FROM:
+			return 'rel:is_derived_from'
+		elif self == EtymWordnetRelation.ETYMOLOGICALLY_RELATED:
+			return 'rel:etymologically_related'
+		elif self == EtymWordnetRelation.ETYMOLOGICAL_ORIGIN_OF:
+			return 'rel:etymology'
+		elif self == EtymWordnetRelation.VARIANT_ORTHOGRAPHY:
+			return 'rel:variant:orthography'
+		else:
+			raise Exception("Unsupported enum: %s" % self)
+
 class EtymWordnetNode:
 	def __init__(self, lang, word):
 		self.lang = lang
 		self.word = word
+
+	def __str__(self):
+		return '(%s, %s)' % (self.lang, self.word)
+
+	def __repr__(self):
+		return 'EWNNode(%s, %s)' % (self.lang, self.word)
 
 class EtymWordnetEdge:
 	def __init__(self, relation, source, target):
@@ -24,19 +42,26 @@ class EtymWordnetEdge:
 		self.source = source
 		self.target = target
 
+	def __str__(self):
+		return '(%s, %s, %s)' % (self.relation, self.source, self.target)
+
+	def __repr__(self):
+		return 'EWNEdge(%s, %s, %s)' % (self.relation, self.source, self.target)
+
 def get_etym_wordnet(relations_to_include=None):
 	etym_wordnet = []
 	parse_errors = []
+	relations_to_include = set(map(
+		EtymWordnetRelation.to_identifier, relations_to_include)) \
+		if relations_to_include else None
 	with open(DATA_PATH, 'r') as f:
 		reader = csv.reader(f, delimiter="\t")
 		for row in reader:
 			try:
 				left_entity, relation, right_entity = row 
-
-				left_entity_lang, left_entity = left_entity.split(':')
-				right_entity_lang, right_entity = right_entity.split(':')
-
 				if not relations_to_include or relation in relations_to_include:
+					left_entity_lang, left_entity = left_entity.split(':')
+					right_entity_lang, right_entity = right_entity.split(':')
 					etym_wordnet.append(EtymWordnetEdge(
 						relation,
 						EtymWordnetNode(left_entity_lang.lower(), left_entity.lower()),
@@ -57,4 +82,14 @@ if __name__ == '__main__':
 	for edge in etym_wordnet:
 		counts_by_relation[edge.relation] += 1
 
+	# {
+	# 	'rel:has_derived_form': 2264591, 
+	# 	'rel:is_derived_from': 2264591, 
+	# 	'rel:etymologically_related': 538480, 
+	# 	'rel:etymological_origin_of': 473415, 
+	# 	'rel:etymology': 473415, 
+	# 	'rel:variant:orthography': 16515, 
+	# 	'rel:derived': 2, 
+	# 	'rel:etymologically': 1
+	# }
 	print(counts_by_relation)
---
+++ train_embeddings.py
@@ -0,0 +1,28 @@
+import argparse
+import numpy as np
+from gensim.models.poincare import PoincareModel
+
+from etymwordnet import get_etym_wordnet, EtymWordnetRelation
+
+# compare euclidean and poincare glove at different dimensionalities
+# things to compare:
+#		1) reconstruction
+#		2) prediction (via model)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Train etymology embeddings')
+	parser.add_argument('--type', dest='type', help='[\'poincare\']')
+	args = parser.parse_args()
+
+	relations = []
+	etym_wordnet = get_etym_wordnet(
+		relations_to_include=[EtymWordnetRelation.IS_DERIVED_FROM])
+	for edge in etym_wordnet:
+		relations.append((
+			'%s-%s' % (edge.source.lang, edge.source.word), 
+			'%s-%s' % (edge.target.lang, edge.target.word)
+		))
+
+	model = PoincareModel(relations[0:1000], size=5, dtype=np.float32)
+	model.train(epochs=1)
+
